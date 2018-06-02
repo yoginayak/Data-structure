13b5250f735d064dd74759c1ea68e6f917126060
yoginayak <38529406+yoginayak@users.noreply.github.com>
2018-06-01 18:12:30 -0700

    Update ds.c

--- ds.c
+++ ds.c
@@ -1,9 +1,6 @@
-// C program for different tree traversals
+
 #include <stdio.h>
 #include <stdlib.h>
- 
-/* A binary tree node has data, pointer to left child
-   and a pointer to right child */
 struct node
 {
      int data;
@@ -11,8 +8,6 @@ struct node
      struct node* right;
 };
  
-/* Helper function that allocates a new node with the
-   given data and NULL left and right pointers. */
 struct node* newNode(int data)
 {
      struct node* node = (struct node*)
@@ -24,56 +19,39 @@ struct node* newNode(int data)
      return(node);
 }
  
-/* Given a binary tree, print its nodes according to the
-  "bottom-up" postorder traversal. */
 void printPostorder(struct node* node)
 {
      if (node == NULL)
         return;
- 
-     // first recur on left subtree
+
      printPostorder(node->left);
- 
-     // then recur on right subtree
      printPostorder(node->right);
- 
-     // now deal with the node
      printf("%d ", node->data);
 }
  
-/* Given a binary tree, print its nodes in inorder*/
 void printInorder(struct node* node)
 {
      if (node == NULL)
           return;
  
-     /* first recur on left child */
      printInorder(node->left);
  
-     /* then print the data of node */
      printf("%d ", node->data);  
- 
-     /* now recur on right child */
-     printInorder(node->right);
+      printInorder(node->right);
 }
  
-/* Given a binary tree, print its nodes in preorder*/
 void printPreorder(struct node* node)
 {
      if (node == NULL)
           return;
  
-     /* first print data of node */
      printf("%d ", node->data);  
  
-     /* then recur on left sutree */
      printPreorder(node->left);  
- 
-     /* now recur on right subtree */
+
      printPreorder(node->right);
 }    
- 
-/* Driver program to test above functions*/
+
 int main()
 {
      struct node *root  = newNode(1);
@@ -322,7 +300,7 @@ void main()
     }
 
     temp->ptr=0;
-    /*reset temp to beginning*/
+  
 
     temp=first;
     printf("\nStatus of the linked list\n");
